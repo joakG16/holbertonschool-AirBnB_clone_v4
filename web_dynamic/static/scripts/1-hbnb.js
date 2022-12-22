2ef8aaa8d928ca088af039cd15a02485273e2478
Joaquin Dominguez <104533476+joakG16@users.noreply.github.com>
2022-12-22 16:37:19 +0000

    found way to change h4

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,13 +1,17 @@
 var checkedIds = [];
+var amenitychecked = [];
 
 $('input[type="checkbox"]').change(function() {
   if ($(this).is(':checked')) {
-    checkedIds.push($(this).data('id')); //accessin dataset property id
-    $('div .amenities h4').html($(this).data('name'));
+    checkedIds.push($(this).data('id')); //accessing dataset property id
+    amenitychecked.push($(this).data('name'));
+    $('div .amenities h4').html(amenitychecked.join(", ")); // join returns a string separted by value specified
   } else {
     var index = checkedIds.indexOf($(this).data('id'));
     if (index > -1) { // if value(the index) is found
       checkedIds.splice(index, 1); // index(start): from where to change the array, 1(deleteCount): number of elements to remove from start
+      amenitychecked.splice(index, 1);
+      $('div .amenities h4').html(amenitychecked.join(", "));
     }
   }
 });
