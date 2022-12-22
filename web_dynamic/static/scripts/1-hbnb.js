631fa0814cf4255db77819beeaaf148e35bb591a
Joaquin Dominguez <104533476+joakG16@users.noreply.github.com>
2022-12-22 16:26:31 +0000

    some changes made

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,14 +1,13 @@
-$(document).ready(function() {
-    var checkedIds = [];
+var checkedIds = [];
 
-    $('.checkbox').change(function() {
-      if ($(this).is(':checked')) {
-        checkedIds.push($(this).prop('id'));
-      } else {
-        var index = checkedIds.indexOf($(this).prop('id'));
-        if (index > -1) {
-          checkedIds.splice(index, 1);
-        }
-      }
-    });
-  });
+$('input[type="checkbox"]').change(function() {
+  if ($(this).is(':checked')) {
+    checkedIds.push($(this).data('id')); //accessing dataset property id
+    $('div .amenities h4').html($(this).data('name'));
+  } else {
+    var index = checkedIds.indexOf($(this).data('id'));
+    if (index > -1) { // if value(the index) is found
+      checkedIds.splice(index, 1); // index(start): from where to change the array, 1(deleteCount): number of elements to remove from start
+    }
+  }
+});
